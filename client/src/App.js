7a918e8c51cc388213f54bb32e721458e1ac35dc
Eugen Soloviov <suenot@gmail.com>
2017-08-23 21:48:21 +0300

    ajax widgets

--- client/src/App.js
+++ client/src/App.js
@@ -10,90 +10,43 @@ class App extends Component {
     super();
     this.state = {
       name: 'React',
-      listItems: '',
-      widgets: ''
-    };
+      listItems: [],
+      widgets: []
+    }
   }
   render() {
-    var listItems;
-    var widgets;
-    axios.get('http://localhost:5000/dns.json', {
-    }).then((response) => {
-      widgets = response.data.widgets;
-      var y = 0;
-      widgets.forEach(function(item, i){
-        item.x = 0;
-        item.y = 0;
-        item.w = 12;
-        item.h = 10;
-        item.name = item.title.split(' ').join('');
-        item.i = i.toString();
-        // i++
-        y+=10;
-      })
-      // this.setState({widgets: widgets});
-      // this.pages = response.data.pages
-      // console.log(widgets)
-      // widgets = widgets;
-      // this.setState({
-      //   listItems: widgets.map((number) =>
-      //     <div key={number.i}>
-      //       <iframe title={number.i} src={'https://cloud.petrusenko.pro/apps/'+number.url} width="100%" height="100%">{number.title}</iframe>
-      //     </div>
-      //   )
-      // });
-      listItems = widgets.map((item) =>
-        <div key={item.i}>
-          <div key={item.i}>
-            <iframe title={item.i} src={'https://cloud.petrusenko.pro/apps/'+item.url} width="100%" height="100%">{item.title}</iframe>
-          </div>
-        </div>
-      );
-      console.log(widgets)
-    }, (err) => {
-      console.log(err);
-    })
-
-    // console.log(this.state.widgets);
-    // var layout = this.state.widgets;
-    // console.log(this.state.widgets)
+    const listItems = this.state.widgets.map((item) =>
+      <div key={item.title.split(' ').join('')} data-grid={{x: item.x, y: item.y, w: item.w, h: item.h}}>
+        <iframe title={item.i} src={'https://cloud.petrusenko.pro/apps/'+item.url} width="100%" height="100%">{item.title}</iframe>
+      </div>
+    );
     return (
       <div>
-        <ReactGridLayout className="layout" layout={widgets} cols={12} rowHeight={30} width={1200}>
+        <ReactGridLayout className="layout" cols={12} rowHeight={30} width={1200} margin={[30,30]}>
           {listItems}
         </ReactGridLayout>
       </div>
     );
   }
   componentWillMount() {
+    axios.get('https://cloud.petrusenko.pro/dashboard/apps', {
     // axios.get('http://localhost:5000/dns.json', {
-    // }).then((response) => {
-    //   let widgets = response.data.widgets
-    //   var y = 0
-    //   widgets.forEach(function(item, i){
-    //     item.x = 0
-    //     item.y = 0
-    //     item.w = 12
-    //     item.h = 10
-    //     item.name = item.title.split(' ').join('')
-    //     item.i = i.toString()
-    //     // i++
-    //     y+=10
-    //   })
-    //   this.setState({widgets: widgets})
-    //   // this.pages = response.data.pages
-    //   // console.log(widgets)
-    //   // widgets = widgets;
-    //   this.setState({
-    //     listItems: widgets.map((item) =>
-    //       <div key={item.i}>
-    //         <iframe title={item.i} src={'https://cloud.petrusenko.pro/apps/'+item.url} width="100%" height="100%">{item.title}</iframe>
-    //       </div>
-    //     )
-    //   });
-    // }, (err) => {
-    //   console.log(err)
-    // })
+    }).then((response) => {
+      var widgets = response.data.widgets;
+      var y = 0;
+      widgets.forEach(function(item, i){
+        item.x = 0
+        item.y = 0
+        item.w = 12
+        item.h = 10
+        item.name = item.title.split(' ').join('')
+        item.i = i.toString()
+        y+=10
+      });
+      this.setState({widgets: widgets});
+    }, (err) => {
+      console.log(err)
+    })
   }
 }
 
